1a7799f1b23b512ae3ae612bb267876cb5dc14a9
jpion8117 <jp8117@gmail.com>
2023-11-16 16:44:49 -0500

    Add project files.

---
+++ ChatrBox/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChatrBox.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
