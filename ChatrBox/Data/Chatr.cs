d1f2f439efaaf957204c9360720c604ae3f81966
jpion8117 <jp8117@gmail.com>
2023-11-16 17:06:53 -0500

    enabled custom user account

---
+++ ChatrBox/Data/Chatr.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ChatrBox.Data
+{
+    public class Chatr : ChatrBox.Data.Chatr
+    {
+    }
+}
---
+++ ChatrBox/Data/Migrations/20231116215844_customUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatrBox.Data.Migrations
+{
+    public partial class customUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
