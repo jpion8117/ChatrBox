309759bb77a83b7e6aa59b8846ff3d830c3c031d
jpion8117 <jp8117@gmail.com>
2023-11-19 02:13:13 -0500

    Added restriction options for reading and posting messages. Wrote a static create function to simplify assigning those restrictions.

--- ChatrBox/Data/CommunityUser.cs
+++ ChatrBox/Data/CommunityUser.cs
@@ -1,4 +1,6 @@
-﻿namespace ChatrBox.Data
+﻿using ChatrBox.Models.CommunityControls;
+
+namespace ChatrBox.Data
 {
     public class CommunityUser
     {
@@ -8,5 +10,56 @@
 
         public virtual int CommunityId { get; set; }
         public virtual Community Community { get; set; }
+
+        public bool CanPost { get; set; }
+        public bool CanRead { get; set; }
+
+        /// <summary>
+        /// Create a community user ready to be inserted into the database with posting 
+        /// controls based on community visibility.
+        /// </summary>
+        /// <param name="communityId">Id of the community the Chat'r is being added to.</param>
+        /// <param name="chatrId">Id of the Chat'r</param>
+        /// <param name="communityVisibility">Visibility setting of the community</param>
+        /// <returns></returns>
+        public static CommunityUser Create(int communityId,  string chatrId, Visibility communityVisibility)
+        {
+            var comUser = new CommunityUser()
+            {
+                Id = communityId,
+                ChatrId = chatrId,
+            };
+
+            switch (communityVisibility)
+            {
+                case Visibility.Closed:
+                    comUser.CanPost = false;
+                    comUser.CanRead = false;
+                    break;
+                case Visibility.Private:
+                    comUser.CanPost = false;
+                    comUser.CanRead = false;
+                    break;
+                case Visibility.Protected:
+                    comUser.CanPost = false;
+                    comUser.CanRead = true;
+                    break;
+                case Visibility.ProtectedPlus:
+                    comUser.CanPost = false;
+                    comUser.CanRead = true;
+                    break;
+                case Visibility.Public:
+                    comUser.CanPost = true;
+                    comUser.CanRead = true;
+                    break;
+                case Visibility.Open:
+                default:
+                    comUser.CanPost = true;
+                    comUser.CanRead = true;
+                    break;
+            }
+
+            return comUser;
+        }
     }
 }
