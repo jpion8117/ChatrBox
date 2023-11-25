3d4bcbe492a511982409f682e0708334c810c631
jpion8117 <jp8117@gmail.com>
2023-11-25 15:29:57 -0500

    Configured the configuration area used for configuring the site's configurable settings.

---
+++ ChatrBox/Areas/Config/Controllers/MainController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatrBox.Areas.Config.Controllers
+{
+    [Area("Config")]
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ChatrBox/Program.cs
+++ ChatrBox/Program.cs
@@ -54,6 +54,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "config",
+    pattern: "{area:exists}/{controller=Main}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
