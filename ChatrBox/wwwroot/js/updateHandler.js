fe83f0c2488b4311d6c236406e48c57b530b1464
jpion8117 <jp8117@gmail.com>
2023-12-01 23:18:25 -0500

    changed message check rate

--- ChatrBox/wwwroot/js/updateHandler.js
+++ ChatrBox/wwwroot/js/updateHandler.js
@@ -2,6 +2,7 @@
 ChatrBoxClient.GetTopics();
 ChatrBoxClient.GetUserStatuses();
 ChatrBoxClient.GetCommunities();
+ChatrBoxClient.Settings.MessageCheckRate = 5000;
 ChatrBoxClient.Interval.Set(
     "messageChecker",
     ChatrBoxClient.Settings.MessageCheckRate,
