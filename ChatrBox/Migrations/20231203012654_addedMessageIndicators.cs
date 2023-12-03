094f60752478cb925fe7fe6eafc29837139c0743
jpion8117 <jp8117@gmail.com>
2023-12-02 20:42:56 -0500

    Added some boolean flags to the message class

---
+++ ChatrBox/Migrations/20231203012654_addedMessageIndicators.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatrBox.Migrations
+{
+    public partial class addedMessageIndicators : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFlaged",
+                table: "Messages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHidden",
+                table: "Messages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
