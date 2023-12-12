295a62db8368a96930d5578d564cb53c5344f6a7
jpion8117 <jp8117@gmail.com>
2023-12-12 12:17:35 -0500

    Added client configuration page accessable to admins. Fixed bug: If user deleted all communities (or didn't have any such as admin) they could not create any communities.

---
+++ ChatrBox/Areas/Config/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using ChatrBox.Areas.Config.Models;
+using ChatrBox.CoreComponents;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatrBox.Areas.Config.Controllers
+{
+    [Area("Config")]
+    [Authorize(Roles = "admin,superAdmin")]
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public IActionResult ClientSettings()
+        {
+            var viewmodel = new ClientSettingsViewModel();
+
+            viewmodel.StatusUpdateSpeed = ConfigManager.StatusUpdateRate;
+            viewmodel.MessageUpdateSpeed = ConfigManager.MessageUpdateRate;
+            viewmodel.MessageDownloadQty = ConfigManager.MessageCount;
+            viewmodel.TimeUntilOffline = ConfigManager.ActivityTimeOut;
+
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public IActionResult SaveConfig(ClientSettingsViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Save();
+                model.SaveSuccessful = true;
+            }
+
+            return View("ClientSettings", model);
+        }
+    }
+}
---
+++ ChatrBox/Areas/Config/Models/ClientSettingsViewModel.cs
@@ -0,0 +1,51 @@
+﻿using ChatrBox.CoreComponents;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChatrBox.Areas.Config.Models
+{
+    public class ClientSettingsViewModel
+    {
+        private bool _saveSuccessful = false;
+        public bool SaveSuccessful 
+        {
+            get
+            {
+                var value = _saveSuccessful;
+                _saveSuccessful = false;
+                return value;
+            }
+
+            set 
+            {
+                _saveSuccessful = value;
+            } 
+        }
+
+        [Required]
+        [Range(3000, 60000)]
+        [Display(Name = "Message Update Speed")]
+        public double MessageUpdateSpeed { get; set; }
+
+        [Required]
+        [Range(20000, 180000)]
+        [Display(Name = "User Status Update Speed")]
+        public double StatusUpdateSpeed { get; set; }
+
+        [Required]
+        [Range(30, 180)]
+        [Display(Name = "Time until user is considered offline")]
+        public int TimeUntilOffline { get; set; }
+
+        [Required]
+        [Range(50, 500)]
+        [Display(Name = "Message Download Quantity")]
+        public int MessageDownloadQty { get; set; }
+        public void Save() 
+        {
+            ConfigManager.ActivityTimeOut = TimeUntilOffline;
+            ConfigManager.StatusUpdateRate = StatusUpdateSpeed;
+            ConfigManager.MessageUpdateRate = MessageUpdateSpeed;
+            ConfigManager.MessageCount = MessageDownloadQty;
+        }
+    }
+}
